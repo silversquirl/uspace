180f45f7c141e4c944fa745f7e420c757d4c1d50
Ellis Trisk-Grove <ellis@trisk-grove.com>
2021-05-12 14:55:35 +0100

    build: add version information to build options
    Reviewed-by: vktec <samadi@vktec.org.uk>

--- build.zig
+++ build.zig
@@ -4,17 +4,37 @@ pub fn build(b: *std.build.Builder) !void {
     const target = b.standardTargetOptions(.{});
     const mode = releaseOptions(b);
 
+    var code: u8 = undefined;
+    const tags = b.execAllowFail(
+        &.{ "git", "describe", "--tags" },
+        &code,
+        std.ChildProcess.StdIo.Ignore,
+    ) catch b.fmt("v0.0.0-{s}", .{try b.execAllowFail(
+        &.{ "git", "rev-parse", "--short", "HEAD" },
+        &code,
+        std.ChildProcess.StdIo.Ignore,
+    )});
+
     // Iterate through the src dir
     var srcDir = try std.fs.cwd().openDir("src", .{ .access_sub_paths = false, .iterate = true });
     defer srcDir.close();
     var iter = srcDir.iterate();
+
     while (try iter.next()) |entry| {
         if (stripSuffix(u8, entry.name, ".zig")) |name| {
             // Add executable to the build
             const exe = b.addExecutable(name, b.fmt("src/{s}", .{entry.name}));
             exe.strip = mode == .ReleaseSmall;
+
+            exe.addBuildOption(
+                []const u8,
+                "tags",
+                tags,
+            );
+
             exe.setTarget(target);
             exe.setBuildMode(mode);
+
             exe.install();
 
             // Add a run step for the executable
