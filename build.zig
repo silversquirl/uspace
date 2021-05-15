3d71c039605e753b974dd6341685f3abedb36e22
Ellis Trisk-Grove <ellis@trisk-grove.com>
2021-05-15 12:13:53 +0100

    build: make changes requested by code review
    Changes to build function:
    - walkPath rather than hardcoding manual directories
    - check the file type when walking (symlink is not handled currently)
    
    Manpage Step:
    - move functions to be after build()
    - use b.fmt instead of concat
    - rename InstallMan to be PreprocessMan
    - dupe dir when initialising
    - use std.debug.print instead of std.debug.warn
    - change debug message as not just copying
    - use buffered readers/writers (for performance reasons)
    - use switch to check errors on read
    - move file copy loop to seperate function (preprocessManpage)
    - addInstallMan takes all of init's options

--- build.zig
+++ build.zig
@@ -1,78 +1,5 @@
 const std = @import("std");
 
-const InstallManStep = struct {
-    step: std.build.Step,
-    builder: *std.build.Builder,
-    src_path: []const u8,
-    dir: std.build.InstallDir,
-    dest_rel_path: []const u8,
-
-    const Self = @This();
-
-    pub fn init(
-        builder: *std.build.Builder,
-        src_path: []const u8,
-        dir: std.build.InstallDir,
-        dest_rel_path: []const u8,
-    ) InstallManStep {
-        builder.pushInstalledFile(dir, dest_rel_path);
-        return InstallManStep{
-            .builder = builder,
-            .step = std.build.Step.init(.InstallFile, builder.fmt("install {s}", .{src_path}), builder.allocator, make),
-            .src_path = builder.dupePath(src_path),
-            .dir = dir,
-            .dest_rel_path = builder.dupePath(dest_rel_path),
-        };
-    }
-
-    fn make(step: *std.build.Step) !void {
-        const self = @fieldParentPtr(InstallManStep, "step", step);
-        const full_src_path = self.builder.pathFromRoot(self.src_path);
-        const full_dest_path = self.builder.getInstallPath(self.dir, self.dest_rel_path);
-        try self.updateMan(full_src_path, full_dest_path);
-    }
-
-    fn updateMan(self: *Self, src_path: []const u8, dest_path: []const u8) !void {
-        const cwd = std.fs.cwd();
-
-        if (self.builder.verbose) {
-            std.debug.warn("cp {s} {s} ", .{ src_path, dest_path });
-        }
-
-        var src_file = try cwd.openFile(self.src_path, .{});
-        defer src_file.close();
-
-        if (std.fs.path.dirname(dest_path)) |dirname| {
-            try cwd.makePath(dest_path);
-        }
-
-        var atomic_file = try cwd.atomicFile(dest_path, .{});
-        defer atomic_file.deinit();
-
-        const reader = src_file.reader();
-        const writer = atomic_file.file.writer();
-
-        while (true) {
-            // TODO check for os string
-            const byte = reader.readByte() catch |err| {
-                if (err == error.EndOfStream) {
-                    break;
-                } else {
-                    return err;
-                }
-            };
-
-            try writer.writeByte(byte);
-        }
-    }
-};
-
-fn addInstallMan(self: *std.build.Builder, path: []const u8) *InstallManStep {
-    const install_step = self.allocator.create(InstallManStep) catch unreachable;
-    install_step.* = InstallManStep.init(self, path, .Prefix, path);
-    return install_step;
-}
-
 pub fn build(b: *std.build.Builder) !void {
     const target = b.standardTargetOptions(.{});
     const mode = releaseOptions(b);
@@ -137,13 +64,14 @@ pub fn build(b: *std.build.Builder) !void {
     }
 
     { // Install manpages
-        inline for ([_][]const u8{"man1"}) |dir| {
-            var manDir = try std.fs.cwd().openDir("man/" ++ dir, .{ .access_sub_paths = false, .iterate = true });
-            defer manDir.close();
-            var iter = manDir.iterate();
-            while (try iter.next()) |entry| {
-                const path = try std.mem.concat(b.allocator, u8, &.{ "man/" ++ dir ++ "/", entry.name });
-                b.getInstallStep().dependOn(&addInstallMan(b, path).step);
+        var walker = try std.fs.walkPath(b.allocator, "man");
+        defer walker.deinit();
+        while (try walker.next()) |entry| {
+            if (entry.kind == .File) {
+                b.getInstallStep().dependOn(&addPreprocessMan(b, entry.path, .Prefix, entry.path, tags).step);
+            } else if (entry.kind == .SymLink) {
+                // TODO uncomment this line when std handles symlinks
+                // b.installFile(entry.path, entry.path);
             }
         }
     }
@@ -154,7 +82,7 @@ fn releaseOptions(b: *std.build.Builder) std.builtin.Mode {
     const small = b.option(bool, "small", "Optimize for size, disable safety checking and strip debug symbols") orelse false;
 
     if (release and small) {
-        std.debug.warn("-Drelease and -Dsmall are mutually exclusive\n\n", .{});
+        std.debug.print("-Drelease and -Dsmall are mutually exclusive\n\n", .{});
         b.invalid_user_input = true;
     } else if (release) {
         return .ReleaseSafe;
@@ -170,4 +98,110 @@ fn stripSuffix(comptime T: type, slice: []const T, suffix: []const T) ?[]const T
     } else {
         return null;
     }
+}
+
+fn dupeInstallDir(self: std.build.InstallDir, builder: *std.build.Builder) std.build.InstallDir {
+    if (self == .Custom) {
+        // Written with this temporary to avoid RLS problems
+        const duped_path = builder.dupe(self.Custom);
+        return .{ .Custom = duped_path };
+    } else {
+        return self;
+    }
+}
+
+const PreprocessManStep = struct {
+    step: std.build.Step,
+    builder: *std.build.Builder,
+    src_path: []const u8,
+    dir: std.build.InstallDir,
+    dest_rel_path: []const u8,
+    version: []const u8,
+
+    const Self = @This();
+
+    pub fn init(
+        builder: *std.build.Builder,
+        src_path: []const u8,
+        dir: std.build.InstallDir,
+        dest_rel_path: []const u8,
+        version: []const u8,
+    ) PreprocessManStep {
+        builder.pushInstalledFile(dir, dest_rel_path);
+        return PreprocessManStep{
+            .builder = builder,
+            .step = std.build.Step.init(.InstallFile, builder.fmt("preproccess and install {s}", .{src_path}), builder.allocator, make),
+            .src_path = builder.dupePath(src_path),
+            .dir = dupeInstallDir(dir), // TODO use std dupe when public
+            .dest_rel_path = builder.dupePath(dest_rel_path),
+            .version = version,
+        };
+    }
+
+    fn make(step: *std.build.Step) !void {
+        const self = @fieldParentPtr(PreprocessManStep, "step", step);
+        const full_src_path = self.builder.pathFromRoot(self.src_path);
+        const full_dest_path = self.builder.getInstallPath(self.dir, self.dest_rel_path);
+        try self.updateMan(full_src_path, full_dest_path);
+    }
+
+    fn updateMan(self: *Self, src_path: []const u8, dest_path: []const u8) !void {
+        const cwd = std.fs.cwd();
+
+        if (self.builder.verbose) {
+            std.debug.print("preprocess {s} and copy to {s}\n", .{ src_path, dest_path });
+        }
+
+        var src_file = try cwd.openFile(src_path, .{});
+        defer src_file.close();
+
+        if (std.fs.path.dirname(dest_path)) |dirname| {
+            try cwd.makePath(dirname);
+        }
+
+        const atomic_file = try std.io.BufferedAtomicFile.create(self.builder.allocator, cwd, dest_path, .{});
+        defer atomic_file.destroy();
+
+        var buffered_reader = std.io.bufferedReader(src_file.reader());
+        try self.preprocessMan(buffered_reader.reader(), atomic_file.writer());
+
+        try atomic_file.finish();
+    }
+
+    fn preprocessMan(self: *Self, reader: anytype, writer: anytype) !void {
+        const search_string = [_]u8{ '\n', '.', 'O', 's', ' ', 'u', 's', 'p', 'a', 'c', 'e' };
+        var search_pos: usize = 0;
+        while (true) {
+            const byte = reader.readByte() catch |err| {
+                switch (err) {
+                    error.EndOfStream => break,
+                    else => |e| return e,
+                }
+            };
+            try writer.writeByte(byte);
+
+            if (search_string[search_pos] == byte) {
+                search_pos += 1;
+            } else {
+                search_pos = 0;
+            }
+
+            if (search_pos == search_string.len) {
+                try writer.writeAll(self.builder.fmt(" {s}", .{self.version}));
+                search_pos = 0;
+            }
+        }
+    }
+};
+
+fn addPreprocessMan(
+    self: *std.build.Builder,
+    src_path: []const u8,
+    dir: std.build.InstallDir,
+    dest_rel_path: []const u8,
+    version: []const u8,
+) *PreprocessManStep {
+    const install_step = self.allocator.create(PreprocessManStep) catch unreachable;
+    install_step.* = PreprocessManStep.init(self, src_path, dir, dest_rel_path, version);
+    return install_step;
 }
